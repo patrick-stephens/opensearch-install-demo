51f67c9f525d7a303b247acb28996262d8148a22
Patrick Stephens <pat@telemetryforge.io>
2026-04-16 11:11:31 +0100

    Add simple run script
    Signed-off-by: Patrick Stephens <pat@telemetryforge.io>

--- init.sh
+++ init.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# This does not work with a symlink to this script
+# SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# See https://stackoverflow.com/a/246128/24637657
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE=$SCRIPT_DIR/$SOURCE
+done
+SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
 DEMO="OpenSearch Easy Install demo"
 AUTHORS="Eric D. Schabell, Patrick Stephens"
 # PROJECT="git@gitlab.com:o11y-workshops/opensearch-install-demo.git"
@@ -22,8 +34,8 @@ export OSD_CONFIG="support/${OSD_APP}.yml"
 export CONTAINER_RUNTIME=${CONTAINER_RUNTIME:-podman}
 
 # importing functions.
-source ./support/functions.sh
-source ./support/colorized.sh
+source "$SCRIPT_DIR/support/functions.sh"
+source "$SCRIPT_DIR/support/colorized.sh"
 
 # wipe screen.
 clear
